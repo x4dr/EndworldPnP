4966f25cd30078ddc9544ec522bb5d3330a83e58
x4dr <x4dr@hotmail.com>
2020-04-27 09:42:23 +0200

    more energy conversion, Speed formula in rulebook

--- serve/initiativetests.py
+++ serve/initiativetests.py
@@ -1,5 +1,7 @@
 import random
 
+import numpy
+
 
 def d10(lvl):
     amt = abs(lvl - 3) + 1
@@ -34,32 +36,35 @@ def newround(actors, actorlevels):
         # print("new round", actors)
 
 
-def manyrolls(amt,lvl):
+def manyrolls(amt, lvl):
     return [d10(lvl) for x in range(amt)]
 
 
-party = {}
-partylevels = {"Lvl1": 1, "Lvl2": 2, "Lvl3": 3, "Lvl4": 4, "Lvl5": 5}
-score = {x: 0 for x in partylevels.keys()}
-rounds = 10000
-for i in range(rounds):
-    newround(party, partylevels)
-    process(party, score)
+def maxspeed(m, p, air_coeff, ground_coeff, base_loss, significant_digits=3):
+    d = m * ground_coeff * 9.81  # kg * 1 * m/s2  ; weight based friction
+    old, vel, airdrag = 0, 10, p / 100  # approximation seeds
+    p -= base_loss
+    while abs(old - vel) > 1 / 10 ** significant_digits:
+        old = vel
+        airdrag = (airdrag + (0.5 * air_coeff * 1.225) * vel ** 2) / 2
+        vel = p / (airdrag + d)
+    return vel
 
-score = {x: score[x] / rounds for x in score.keys()}
-print("average number of actions per round:", score)
 
-avgdiff = 0
-hits = 0
+res = []
+for m in range(10):
+    res.append([])
+    for p in range(500):
+        res[-1].append(maxspeed((m + 2) * 5000, (p * 10000), 5, 0.12, 0))
 
-print("average number for the levels:", sum(manyrolls(1000,1))/1000,
-      sum(manyrolls(1000, 2)) / 1000,
-      sum(manyrolls(1000, 3)) / 1000,
-      sum(manyrolls(1000, 4)) / 1000,
-      sum(manyrolls(1000, 5)) / 1000,)
-for i in range(rounds):
-    a = d10(3)
-    b = d10(3) - 1
-    hits += 1 if b >= a else 0
-    avgdiff += b - a if a <= b else 0
-print(avgdiff / hits, hits / rounds)
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    i = 0
+    for x in res:
+        plt.plot(x, label=f"{(i+2)*5} t")
+        i += 1
+        plt.legend()
+    plt.plot(list(10*numpy.log(x) for x in range(500)))
+    plt.show()
